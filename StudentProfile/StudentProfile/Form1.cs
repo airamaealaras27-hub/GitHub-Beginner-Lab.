2c773bd54c96e620df9c42b5d60fb28cba14acf6
Aira-Mae-Alaras <airamaealaras27@gmail.com>
2026-09-14 20:32:15 +0800

    Implement receive notification (#3)

--- StudentProfile/StudentProfile/Form1.cs
+++ StudentProfile/StudentProfile/Form1.cs
@@ -1,10 +1,32 @@
+using System;
+using System.Windows.Forms;
+
 namespace StudentProfile
 {
     public partial class Form1 : Form
     {
+        int currentNumber = 10000;
+        int userNumber = 10004;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            currentNumber++;
+
+            int difference = userNumber - currentNumber;
+
+            if (difference <= 3 && difference > 0)
+            {
+                MessageBox.Show(
+                    "Your turn is near!\nYour number is P - " + userNumber,
+                    "LineUpNow",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -12,4 +34,8 @@ namespace StudentProfile
             MessageBox.Show("Student:");
         }
     }
-}
+
+    }
+
+
+
